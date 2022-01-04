e921b4cb6a7581de27fc23cd022f98c1ebfc4f3d
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-01-04 14:44:29 -0500

    show book title,author,img on site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,9 @@
-// var getBooks = function(response) {
-//     fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=K1GxuJ1AkeAHy6BPRuFAGpG9GmITR0Eu")
-//     .then(response)
-// }
-
-// var response = fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=K1GxuJ1AkeAHy6BPRuFAGpG9GmITR0Eu")
-// .then(function(response) {
-//     response.json().then(function(data){
-//         console.log(data)
-//     })
-// })
+var mainEl = document.querySelector(".search")
 
 var getBooks = function(category) {
     // dynamic api url
     var category = "hardcover-fiction"
     var apiUrl = "https://api.nytimes.com/svc/books/v3/lists/current/" + category + ".json?api-key=K1GxuJ1AkeAHy6BPRuFAGpG9GmITR0Eu"
-    // var apiUrl = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=K1GxuJ1AkeAHy6BPRuFAGpG9GmITR0Eu"
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
@@ -29,10 +18,23 @@ var getBooks = function(category) {
 // loop over pulled data 
 var bookLoop = function(data) {
     for (i=0; i<data.results.books.length; i++) {
+        // collect the variables we need
         var title = data.results.books[i].title
         var author = data.results.books[i].author
         var photo = data.results.books[i].book_image
-        console.log(title)
+        
+        // then create element html element and add api data to it
+        var titleEl = document.createElement("div")
+        titleEl.textContent = "Book Title: " + title
+        var authorEl = document.createElement("div")
+        authorEl.textContent = "Author: " + author
+        var img = document.createElement("img")
+        img.src = photo
+        // div to hold all book elements
+        var bookEl = document.createElement("div")
+        // // append elements 
+        bookEl.append(img, titleEl, authorEl)
+        mainEl.appendChild(bookEl)
     }
 }
 var displayBooks = function(data, category){
