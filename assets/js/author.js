41320b598eb1380ed16bda02e99fa3efc581a083
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-01-10 14:41:00 -0500

    ability to search by author and append results to page

--- assets/js/author.js
+++ assets/js/author.js
@@ -24,31 +24,12 @@ var authorLoop = function(data){
     for (i=0; i<30; i++){
         var titleEl = data.docs[i].title
         var authorEl = data.docs[i].author_name
-        var isbn = data.docs[i].isbn[10]
+        // var description = data.docs[i]
+        var isbn = data.docs[i].isbn[0]
         // run isbn through second api to grab book cover
         coverLoop(isbn, titleEl, authorEl)
 }
 }
-// var dispCover = function(url){
-//     // create elements to display data on page
-//     var card = document.createElement("div")
-//     var cardImg = document.createElement("div")
-//     var fig = document.createElement("figure")
-//     var img = document.createElement("img")
-//     var col = document.createElement("div")
-//     // set values
-//     img.src = url
-//     card.classList = "card"
-//     cardImg.classList = "card-image"
-//     fig.classList = "image is-2by3"
-//     col.classList = "column is-3"
-//     // append elements to page
-//     fig.appendChild(img)
-//     cardImg.appendChild(fig)
-//     card.appendChild(cardImg)
-//     col.appendChild(card)
-//     resultsEl.appendChild(col)
-// }
 
 var coverLoop = function(isbn, title, author){
     apiUrl = "https://covers.openlibrary.org/b/isbn/" + isbn + "-M.jpg"
@@ -60,58 +41,39 @@ var coverLoop = function(isbn, title, author){
         var cardImg = document.createElement("div")
         var fig = document.createElement("figure")
         var img = document.createElement("img")
+        var cardCont = document.createElement("div")
+        var media = document.createElement("div")
+        var mediaCont = document.createElement("div")
+        var titleEl = document.createElement("p")
+        var authorEl = document.createElement("p")
         var col = document.createElement("div")
+
         // set values
         img.src = url
+        titleEl.textContent = title 
+        authorEl.textContent = author
+
+        // create class names
         card.classList = "card"
         cardImg.classList = "card-image"
         fig.classList = "image is-2by3"
+        cardCont.classList = "card-content"
+        mediaCont.classList ="media-content"
+        media.classList = "media"
+        titleEl.classList = "title is-4 zzz"
+        authorEl.classList = "subtitle is-6"
         col.classList = "column is-3"
+
         // append elements to page
         fig.appendChild(img)
         cardImg.appendChild(fig)
-        card.appendChild(cardImg)
+        mediaCont.append(titleEl,authorEl)
+        media.appendChild(mediaCont)
+        cardCont.appendChild(media)
+        card.append(cardImg,cardCont)
         col.appendChild(card)
         resultsEl.appendChild(col)
     })
-        // // create elements to display data on page
-        // var card = document.createElement("div")
-        // var cardImg = document.createElement("div")
-        // var fig = document.createElement("figure")
-        // var img = document.createElement("img")
-        // var cardCont = document.createElement("div")
-        // var media = document.createElement("div")
-        // var mediaCont = document.createElement("div")
-        // var titleEl = document.createElement("p")
-        // var authorEl = document.createElement("p")
-        // var col = document.createElement("div")
-    
-        // // set values
-        // // img.src = url
-        // authorEl.textContent = author
-        // titleEl.textContent = title
-        // // titleText.setAttribute("id", "styleTitle")
-        
-        // // create class names
-        // card.classList = "card"
-        // cardImg.classList = "card-image"
-        // fig.classList = "image is-2by3"
-        // cardCont.classList = "card-content"
-        // mediaCont.classList ="media-content"
-        // media.classList = "media"
-        // title.classList = "title is-4 zzz"
-        // author.classList = "subtitle is-6"
-        // col.classList = "column is-3"
-    
-        // // append elements to page
-        // fig.appendChild(img)
-        // cardImg.appendChild(fig)
-        // mediaCont.append(title,author)
-        // media.appendChild(mediaCont)
-        // cardCont.appendChild(media)
-        // card.append(cardImg,cardCont)
-        // col.appendChild(card)
-        // resultsEl.appendChild(col)
 }
 
 
